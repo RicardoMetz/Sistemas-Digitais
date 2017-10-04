34ec80bda57ca1d07a9984f84f28cd16bc383c8d
RicardoMetz <ricardo_metz@hotmail.com>
2017-10-04 20:54:31 -0300

    Update 1 - piscaLeds.v

--- 1 - piscaLeds.v
+++ 1 - piscaLeds.v
@@ -1,3 +1,5 @@
+//ALUNOS: DEIVIDI LUIZ, RICARDO METZ WEITZ
+
 //Funcionamento: Velocidade já inicia normal, com tempo de um segundo
 //(KEY0 velocidade normal, KEY1 velocidade multiplicada por dois e sucessivamente com KEY2 e KEY3).
 
